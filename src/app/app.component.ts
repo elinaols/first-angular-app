de0f44b7f2715db56cc25b13ee05f752021fabe5
elinaols <elol0031@student.hv.se>
2025-01-26 20:36:44 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RestaurantsInfoComponent } from '../app/restaurants-info/restaurants-info.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // Imports modules and components to make them available in the application
+  imports: [RouterOutlet, RestaurantsInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'restaurants-angular';
-}
+
+export class AppComponent {}
